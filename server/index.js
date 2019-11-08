dbad4593f28bb3e98b517edc122d028738d1af42
aniruddhchouksey <choukseyaniruddh@gmail.com>
2019-11-09 01:03:13 +0530

    the writing part for the application code is over now we will work on dockerising the application

---
+++ server/index.js
@@ -0,0 +1,77 @@
+const keys = require('./keys');
+
+// Express setup
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors)
+
+// Setting up the postgres client
+
+const {
+    Pool
+} = require('pg');
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+});
+
+pgClient.on('error', () => console.log("lost connection to pg"));
+
+pgClient
+    .query('CREATE TABLE IF NOT EXIST values (number INT)')
+    .catch(err => console.log(err));
+
+// REDIS CLIENT CONFIGURATION
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+// Creating a duplicate to diffrenciate bw subscribe and publish redis collection
+const redisPublisher = redisClient.duplicate();
+
+// EXPRESS ROUTE HANDLERS
+
+app.get('/', (req, res) => {
+    res.send('Hi');
+});
+
+app.get('/values/all', async (req, res) => {
+    const values = await pgClient.query('SELECT * FROM values');
+    res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+    redisClient.hgetall('values', (err, values) => {
+        res.send(values);
+    });
+});
+
+app.post('/values', async (req, res) => {
+    const index = req.body.index;
+    if (parseInt(index) > 40) {
+        return res.status(422).send('Index too high');
+    }
+
+    redisClient.hset('values', index, 'Nothing Yet!');
+    redisPublisher.publish('insert', index);
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+    res.send({
+        working: true
+    });
+
+});
+
+app.listen(5000, err => {
+    console.log('listening');
+})
